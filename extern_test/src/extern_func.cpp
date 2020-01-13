42211724d393c8028f4393a538ab1eaf02525aaf
lookingfeng <fengzhangweiwei@gmail.com>
2020-01-13 17:57:53 +0800

    增加——extern关键字的使用

---
+++ extern_test/src/extern_func.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: lookingfeng
+ * @Date: 2020-01-13 17:48:10
+ * @LastEditTime : 2020-01-13 17:50:55
+ * @Description: 
+ */
+
+#include <iostream>
+using namespace std;
+
+void test_extern_func()
+{
+    cout<<"print msg from other file!"<<endl;
+}
---
+++ extern_test/src/main.cpp
@@ -0,0 +1,7 @@
+
+extern void test_extern_func();
+
+int main(int argc, char *argv[]) {
+	test_extern_func();
+	return 0;
+}
